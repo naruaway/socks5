16eb7087e400a67ac9474e1c28de6f5a2623fd77
Narutaka Awaya <naruaway@naruaway.com>
2017-03-08 22:15:47 +0900

    Relax some conditions

--- socks5/connection.py
+++ socks5/connection.py
@@ -69,11 +69,8 @@ class _ClientConnection(object):
                 self.machine.set_state("end")
 
         elif self.state == 'response':
-            if (self._version != 5 or
-               self._addr_type != current_event.atyp or
-               self._addr != current_event.addr or
-               self._port != current_event.port):
-                    raise ProtocolError("ClientConnection:recv: receive incorrect data from server")
+            if self._version != 5:
+                raise ProtocolError("ClientConnection:recv: receive incorrect data from server")
             self.machine.set_state('end')
 
         return current_event
